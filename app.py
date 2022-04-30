98da9f7887e9feec23879d3f2eeb4150b72112f4
Vasilis CodeX <vasilis.tris@gmail.com>
2022-04-30 18:16:07 +0300

    change port of api & folder structure

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from dotenv import dotenv_values
+
+config = dotenv_values("./config/.env")
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-   return 'Hello World'
+    return 'Hello World'
+
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(host='0.0.0.0', port=config['API_PORT'], debug=True)
